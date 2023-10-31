7e5cd0e5847c424b4bb424ff7d6486ea9d231403
nourhan <nourhansaeed89@gmail.com>
2023-10-31 10:43:23 -0700

    loop

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+def uppercase(str):
+    for char in str:
+        if ord('a') <= ord(char) <= ord('z'):
+            print("{}".format(chr(ord(char) - 32)), end='')
+        else:
+            print("{}".format(char), end='')
+    print()
