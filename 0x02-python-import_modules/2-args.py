054575e38c9fef32e467de6bfbd0c08f12e870ce
nourhan <nourhansaeed89@gmail.com>
2023-11-02 13:48:55 -0700

    modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
 from sys import argv
 
-if __name__ == "__main__":
     length = len(argv) - 1
     if length == 0:
         print("{:d} arguments.".format(length))
@@ -12,4 +12,3 @@ if __name__ == "__main__":
         print("{:d} arguments:".format(length))
         for i in range(1, length + 1):
             print("{:d}: {}".format(i, argv[i]))
-
