2661ee3d909ec703612bfee854e6a85918537ec0
nourhan <nourhansaeed89@gmail.com>
2023-11-08 13:19:38 -0800

    st

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    new = a_dictionary.copy()
-    for k, val in a_dictionary.items():
+    new_dict = a_dictionary.copy()
+    for key, val in a_dictionary.items():
         if val == value:
-            del new[k]
-    return new
+            del new_dict[key]
+    return new_dict
