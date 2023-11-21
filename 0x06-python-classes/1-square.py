b97910b9d5a153f35e1b807327f0ae9c36a355e3
nourhan <nourhansaeed89@gmail.com>
2023-11-21 10:52:58 -0800

    class

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -8,6 +8,6 @@ class Square:
         """Initializes a new Square instance.
 
         Parameters:
-            size (int): The size of the square.
+            size: The size of the square.
         """
         self.__size = size
