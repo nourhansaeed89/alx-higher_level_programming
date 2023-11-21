2cbe66a3538984d68a39d43ae1488fff9de3dcf1
nourhan <nourhansaeed89@gmail.com>
2023-11-21 12:35:14 -0800

    class

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,11 @@
-#!/usr/bin/python3
+!/usr/bin/python3
 """This module defines a Square class."""
+
 
 class Square:
     """Define a square"""
 
-    def __init__(self, size):
+    def __init__(self, size=0):
         """Constructor.
 
         Args:
