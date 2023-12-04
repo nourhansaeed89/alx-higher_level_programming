6fb530d3135f8090d5105dd256453ffcaa6d9c7d
nourhan <nourhansaeed89@gmail.com>
2023-12-04 05:20:02 -0800

    inheritance

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''Module for ikind of class method.'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''Determin  if an object is in class or not.'''
+    return isinstance(obj, a_class)
