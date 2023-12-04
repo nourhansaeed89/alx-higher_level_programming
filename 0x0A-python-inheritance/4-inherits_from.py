657a5cb6258937f6c97a48a745bf090cc57b75e8
nourhan <nourhansaeed89@gmail.com>
2023-12-04 05:25:14 -0800

    inheritance

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''Module for inherits_from method.'''
+
+
+def inherits_from(obj, a_class):
+    '''Determi if an object is a true of class.'''
+    return isinstance(obj, a_class) and type(obj) != a_class
