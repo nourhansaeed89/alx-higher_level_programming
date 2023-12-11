2ed97efa57a53b3f867a616d56daf3a592a815bc
nourhan <nourhansaeed89@gmail.com>
2023-12-11 11:13:06 -0800

    test

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""Module for Rectangle class"""
+
+
+from models.base import Base
+
+class Rectangle(Base):
+    """Rectangle class, inherits from Base"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Class constructor for Rectangle"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """Getter for width attribute"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter for width attribute"""
+        self.validate_non_negative_int("width", value)
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter for height attribute"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter for height attribute"""
+        self.validate_non_negative_int("height", value)
+        self.__height = value
+
+    @property
+    def x(self):
+        """Getter for x attribute"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Setter for x attribute"""
+        self.validate_non_negative_int("x", value)
+        self.__x = value
+
+    @property
+    def y(self):
+        """Getter for y attribute"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Setter for y attribute"""
+        self.validate_non_negative_int("y", value)
+        self.__y = value
+
+    def validate_non_negative_int(self, attribute, value):
+        """Validate that the given value is a non-negative integer"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(attribute))
+        if value < 0:
+            raise ValueError("{} must be >= 0".format(attribute))
