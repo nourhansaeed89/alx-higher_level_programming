e99ac0410e13c8563b502af314be83a722c29a92
nourhan <nourhansaeed89@gmail.com>
2023-12-25 05:57:37 -0800

    input

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module for writing a text file."""
 
+
 def write_file(filename="", text=""):
     """Write a string to a text file and return the number of characters written."""
     with open(filename, 'w', encoding='utf-8') as file:
