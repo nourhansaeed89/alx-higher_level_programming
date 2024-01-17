8df7626ea30e85a1fbfb51e956b03bb4f2fccb37
nourhan <nourhansaeed89@gmail.com>
2024-01-17 14:33:27 -0800

    sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,7 +3,10 @@ CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
 	IDENTIFIED BY 'user_0d_1_pwd';
 
 
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES
+   ON *.*
+   TO 'user_0d_1'@'localhost'
+   IDENTIFIED BY 'user_0d_1_pwd';
 
 
 FLUSH PRIVILEGES;
