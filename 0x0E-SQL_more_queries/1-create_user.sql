f19c7d5098e372883b98ae82a237b984581e4fad
nourhan <nourhansaeed89@gmail.com>
2024-01-17 15:05:16 -0800

    sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,9 +1,9 @@
--- creat the MySQL server user user_0d_1.
-CREATE USER
-    IF NOT EXISTS 'user_0d_1'@'localhost'
+-- Create user user_0d_1 if not exists
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
     IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES
-   ON *.*
-   TO 'user_0d_1'@'localhost'
-   IDENTIFIED BY 'user_0d_1_pwd';
+
+
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+
 FLUSH PRIVILEGES;
--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,11 +1,5 @@
 -- Create table unique_id if not exists
-USE `hbtn_0d_2`;
-
-CREATE TABLE IF NOT EXISTS unique_id (
-    id INT DEFAULT 1 UNIQUE,
-    name VARCHAR(256),
-    PRIMARY KEY (id)
+CREATE TABLE IF NOT EXISTS `unique_id` (
+    `id`   INT		DEFAULT 1 UNIQUE,
+    `name` VARCHAR(256)
 );
-
-
-INSERT INTO unique_id (id, name) VALUES (89, 'Best School');
