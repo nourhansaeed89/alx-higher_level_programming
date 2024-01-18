2c6b300817d91b9a82b1d1218c9fffbed9c96f1e
nourhan <nourhansaeed89@gmail.com>
2024-01-18 00:44:07 -0800

    sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,16 +1,7 @@
 -- creates the database hbtn_0d_usa and the table states
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
--- Switch to the database hbtn_0d_usa
-USE hbtn_0d_usa;
-
--- Create table cities if not exists
-CREATE TABLE IF NOT EXISTS cities (
-    id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
-    state_id INT NOT NULL,
-    name VARCHAR(256) NOT NULL,
-    FOREIGN KEY (state_id) REFERENCES states(id)
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
+    PRIMARY KEY(`id`),
+    `id`   INT          NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(256) NOT NULL
 );
-
--- Insert sample data
-INSERT INTO cities (state_id, name) VALUES (1, 'San Francisco');
