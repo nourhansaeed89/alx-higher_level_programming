9f2320be6b6aabb82a3af46d11e370b32159729d
nourhan <nourhansaeed89@gmail.com>
2024-02-10 12:02:13 -0800

    sql

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,4 @@
+-- This script counts the number of records with id = 89 in the table first_table.
+
+-- Count the number of records with id = 89 in first_table.
+SELECT COUNT(*) FROM first_table WHERE id = 89;
