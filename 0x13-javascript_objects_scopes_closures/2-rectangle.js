37662c98bcfcc41bb7c2c056582a5cedf1bb1bcb
nourhan <nourhansaeed89@gmail.com>
2024-02-13 12:20:36 -0800

    java

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 // class that defines a rectangle
 class Rectangle {
-  constructor(w, h) {
-    if (w <= 0 || h <= 0) {
-      return {};
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
-    this.width = w;
-    this.height = h;
   }
 }
+
+module.exports = Rectangle;
