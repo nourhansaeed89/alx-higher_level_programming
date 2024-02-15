0010589ae18e27e8dd4edf9bbb48f7c1126b5853
nourhan <nourhansaeed89@gmail.com>
2024-02-15 12:12:37 -0800

    java

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,19 +1,10 @@
 #!/usr/bin/node
-class Rectangle {
-  constructor(w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    } else {
-      return {};
-    }
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
 
-  print() {
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-    }
+  print () {
+    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
   }
-}
-
-module.exports = Rectangle;
+};
