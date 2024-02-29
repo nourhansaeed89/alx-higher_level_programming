a8e291ea19834b337482c2e485ee86dd87e194c9
nourhan <nourhansaeed89@gmail.com>
2024-02-29 06:25:54 -0800

    n

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 # This script takes a URL as input, sends a request to that URL using curl.
 
-# Send a GET request to the URL and save the response body to a temporary file
-curl -s "$1" > temp_body.txt
-
-# Get the size of the body of the response in bytes and display it
-wc -c < temp_body.txt
-
-# Remove the temporary file
-rm temp_body.txt
+curl -sI $1 | grep "Content-Length" | cut -d " " -f2
